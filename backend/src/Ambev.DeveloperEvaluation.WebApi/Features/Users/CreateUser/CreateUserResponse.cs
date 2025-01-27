d4876a2dcd5b2d4a362d77a9804122ad4a839444
Guilherme Rodrigues <guilhermejonathan@hotmail.com>
2025-01-27 10:49:29 -0300

    update CreatUser

--- backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Users/CreateUser/CreateUserResponse.cs
+++ backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Users/CreateUser/CreateUserResponse.cs
@@ -11,29 +11,4 @@ public class CreateUserResponse
     /// The unique identifier of the created user
     /// </summary>
     public Guid Id { get; set; }
-
-    /// <summary>
-    /// The user's full name
-    /// </summary>
-    public string Username { get; set; } = string.Empty;
-
-    /// <summary>
-    /// The user's email address
-    /// </summary>
-    public string Email { get; set; } = string.Empty;
-
-    /// <summary>
-    /// The user's phone number
-    /// </summary>
-    public string Phone { get; set; } = string.Empty;
-
-    /// <summary>
-    /// The user's role in the system
-    /// </summary>
-    public UserRole Role { get; set; }
-
-    /// <summary>
-    /// The current status of the user
-    /// </summary>
-    public UserStatus Status { get; set; }
 }
